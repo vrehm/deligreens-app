21f452b45136627b98b21d60376ffd196add23ae
Vincent REHM <vincentrehm@yahoo.fr>
2016-08-16 21:57:39 +0200

    working js for categories and types, very dirty"

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
   validates :name, :description, :stock, :price, :weight, :category, :variety, :start_season, :end_season, :user_id, :published, presence: true
+
+  TYPES = [['Fruit', 'Légume'], ['Oeuf', 'Fromage', 'Lait'], ['Viande', 'Poisson'], ['Bière', 'Jus', 'Sirop']]
 end
