cd137e7d6b2f7fcab57b332d08b2740c40dcf8d1
Lucas Geitner <Lucas.Geitner@outlook.com>
2016-08-18 16:32:38 +0200

    Voilà :)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,11 +5,16 @@ class OrdersController < ApplicationController
 
   def create
     ## find in the bouton show.
-    @order = Order.new(order_param)
-    # @order.user = params[:order][:user_id]
-    raise
-    # @order.product = params[:order][:product_id]
-
+     @order = Order.new
+     @order.user = User.find(params[:order][:user])
+     @order_items = OrderItem.new
+     @order_items.product = Product.find(params[:order][:product])
+     @order_items.order = @order
+     @order_items.quantity = params[:order][:amount]
+     @price = @order_items.product.price
+     @order.amount += @price * @order_items.quantity
+     @order.order_items << @order_items
+     raise
   end
   private
 
