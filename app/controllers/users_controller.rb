2598da92af3c5d2fffdddf4580b5bea5a5b8be6a
Hedi Jabri <hedi.jbi@gmail.com>
2016-08-18 22:47:43 +0200

    Add pending orders in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @products = Product.where(user_id: @user)
+    @current_order = Order.where(user_id: current_user, current_order: true)
+    @pending_orders = Order.where(user_id: current_user, pending_order: true)
     @producers = User.where(producer: true)
+
   end
 
   def dashboard
