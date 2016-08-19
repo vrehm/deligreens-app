f5965daa86163ef680f874961338877a19df740f
Hedi Jabri <hedi.jbi@gmail.com>
2016-08-19 11:23:48 +0200

    Add validate orders to user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @current_order = Order.where(user_id: current_user, current_order: true)
     @pending_orders = Order.where(user_id: current_user, pending_order: true)
+    @validate_orders = Order.where(user_id: current_user, validate_order: true)
     @producers = User.where(producer: true)
 
   end
